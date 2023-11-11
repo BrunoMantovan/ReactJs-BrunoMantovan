a0e34e090f51bd8cb8b9ef869a7bde34d749e395
BrunoMantovan <brunochupetin1@gmail.com>
2023-11-11 19:23:55 -0300

    responsive navbar

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import estilos from "./ItemListContainer.module.css"
+import axios from "axios"
+
+
 
 export default function ItemListContainer({titulo}) {
+  const url = "https://swapi.dev/api/starships/?page="
+  const [Data, SetData] = useState([]);
+  useEffect(()=>{
+    axios.get(url + "1")
+    .then(({data})=> console.log(data.results))
+  }, [])
+
   return (
     <div className={estilos.div}>
-        <h1>{titulo}</h1>
+      <h1>{titulo}</h1>
     </div>
   )
 }
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import estilos from "./Navbar.module.css"
 import Cartwidget from '../CartWidget/Cartwidget'
 
 export default function Navbar() {
+
+const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
+  const closeMenu = () => setClick(false)
+
   return (
     <nav className={estilos.navbar}>
-        <h1>Ecommerce</h1>
-        <ul className={estilos.list}>
-            <li><a href="">Celulares</a></li>
-            <li><a href="">Televisores</a></li>
-            <li><a href="">Consolas</a></li>
-            <li><a href="">Gaming</a></li>
-        </ul>
+      <a href=""><h1>Ecommerce</h1></a>
+
+      
+      <ul className={click ? `${estilos.active} ${estilos["nav-menu"]}` : estilos["nav-menu"]}>
+        <li onClick={closeMenu}><a href="">Celulares</a></li>
+        <li onClick={closeMenu}><a href="">Televisores</a></li>
+        <li onClick={closeMenu}><a href="">Consolas</a></li>
+        <li onClick={closeMenu}><a href="">Gaming</a></li>
+      </ul>
+      <div className={estilos.menu}>
         <Cartwidget/>
+        <div className={estilos['menu-icon']} onClick={handleClick}>
+          <i className={`${click ? "fas fa-times" : "fas fa-bars"} ${click ? estilos['fa-times'] : estilos['fa-bars']}`} ></i>
+      </div>
+      </div>
+     
     </nav>
   )
 }
